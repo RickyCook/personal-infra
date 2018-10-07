7e08d0d09c8c2fc13e6c917759ef4524bcea27d3
Ricky Cook <ricky.cook@bluebike.com.au>
2018-10-07 14:10:34 +1100

    Only try install docker if it doesn't exist

--- sync.sh
+++ sync.sh
@@ -2,11 +2,13 @@
 THISDIR="$(cd "$(dirname "$0")"; pwd)"
 rsync --archive "$THISDIR/root/" /
 
-dnf -y install dnf-plugins-core
-dnf config-manager \
-    --add-repo \
-    https://download.docker.com/linux/fedora/docker-ce.repo
-dnf -y install docker-ce
+if [[ -f /usr/bin/docker ]]; then
+    dnf -y install dnf-plugins-core
+    dnf config-manager \
+        --add-repo \
+        https://download.docker.com/linux/fedora/docker-ce.repo
+    dnf -y install docker-ce
+fi
 
 systemctl daemon-reload
 
