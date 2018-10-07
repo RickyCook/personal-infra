dba80fdcc12de43846c0dca4bad5df98d6743d2d
Ricky Cook <ricky.cook@bluebike.com.au>
2018-10-07 14:01:10 +1100

    Ensure systemctl services are enabled and running

--- sync.sh
+++ sync.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 THISDIR="$(cd "$(dirname "$0")"; pwd)"
 rsync --archive "$THISDIR/root/" /
+
+systemctl daemon-reload
+
+for op in enable start; do
+    systemctl $op nginx-proxy
+    systemctl $op nginx-proxy-letsencrypt
+done
