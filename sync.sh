20aa83f166c3b45a2df527a4c9ac15f4c0658c52
Ricky Cook <ricky.cook@bluebike.com.au>
2018-10-07 15:04:05 +1100

    Add file download

--- sync.sh
+++ sync.sh
@@ -1,8 +1,32 @@
 #!/bin/bash
-THISDIR="$(cd "$(dirname "$0")"; pwd)"
-rsync --archive "$THISDIR/root/" /
+set -e
 
-if [[ ! -f /usr/bin/docker ]]; then
+THISDIR="$(cd "$(dirname "$0")"; pwd)"
+
+which jq >/dev/null || dnf -y install jq
+
+mkdir -p "$THISDIR/files"
+filescount=$(jq 'keys|length' "$THISDIR/files.json")
+for n in $(seq 0 $(expr $filescount - 1)); do
+    set -x
+    n_name="$(jq -r "keys[$n]" "$THISDIR/files.json")"
+    n_url="$(jq -r ".[\"$n_name\"].url" "$THISDIR/files.json")"
+    n_md5="$(jq -r ".[\"$n_name\"].md5" "$THISDIR/files.json")"
+    n_path="$THISDIR/files/$n_name"
+
+    if [[ ! -f "$n_path" ]] || ! (echo "$n_md5 $n_path" | md5sum --check); then
+        curl --fail --output "$n_path" "$n_url"
+        if ! echo "$n_md5 $n_path" | md5sum --check; then
+            echo Failed to verify $n_name >&2
+            exit 1
+        fi
+    fi
+done
+exit
+
+rsync --archive "$THISDIR/root/" /w
+
+if ! which docker >/dev/null; then
     dnf -y install dnf-plugins-core
     dnf config-manager \
         --add-repo \
