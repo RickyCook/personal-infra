06fa350911d1b749cf4b4f732afd3f0c5deab822
Ricky Cook <ricky.cook@bluebike.com.au>
2018-10-07 14:03:14 +1100

    Install packages

--- sync.sh
+++ sync.sh
@@ -2,6 +2,12 @@
 THISDIR="$(cd "$(dirname "$0")"; pwd)"
 rsync --archive "$THISDIR/root/" /
 
+dnf -y install dnf-plugins-core
+dnf config-manager \
+    --add-repo \
+    https://download.docker.com/linux/fedora/docker-ce.repo
+dnf -y install docker-ce
+
 systemctl daemon-reload
 
 for op in enable start; do
